670d0e36ed39baaacd9cd0d95d27492a4d27496a
sidhujag <sidhujag@hotmail.com>
2018-08-14 14:53:58 -0700

    change def

--- src/udp.h
+++ src/udp.h
@@ -1,8 +1,8 @@
 // Copyright (c) 2013 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-#ifndef BITCOIN_UDP_H
-#define BITCOIN_UDP_H
+#ifndef SYSCOIN_UDP_H
+#define SYSCOIN_UDP_H
 
 #include <string>
 #include "net.h"
@@ -12,4 +12,4 @@ void ThreadUDPServer();
 
 bool SendUDPMessage(CNode *pfrom, std::string strCommand, std::vector<CInv> &vInv);
 
-#endif // BITCOIN_UDP_H
+#endif // SYSCOIN_UDP_H
