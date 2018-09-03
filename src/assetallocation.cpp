75e25b742d688bca4a63c1ed2ec4767dd1ed9dba
sidhujag <sidhujag@gmail.com>
2018-09-03 16:06:52 -0700

    only save test time on receivers

--- src/assetallocation.cpp
+++ src/assetallocation.cpp
@@ -730,7 +730,7 @@ bool CheckAssetAllocationInputs(const CTransaction &tx, const CCoinsViewCache &i
 		int64_t ms = INT64_MAX;
 		if (fJustCheck) {
 			ms = GetTimeMillis();
-			if(fTPSTest)
+			if(fTPSTest && !fTPSTestEnabled)
 				vecTPSTestReceivedTimes.emplace_back(theAssetAllocation.txHash, GetTimeMicros());
 		}
 		const string &user = op == OP_ASSET_COLLECT_INTEREST ? user1 : "";
