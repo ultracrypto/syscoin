fc7c1a21d14ac0fe04223c9324c7a7f52da927dc
sidhujag <sidhujag@gmail.com>
2018-09-18 19:02:32 -0700

    update param

--- src/escrow.cpp
+++ src/escrow.cpp
@@ -1730,7 +1730,8 @@ UniValue escrowcreaterawtransaction(const JSONRPCRequest& request) {
 		}
 		arrayCreateParams.push_back(recpArray);
 		arrayCreateParams.push_back("memo");
-		arrayCreateParams.push_back(UniValue::VNULL);
+		UniValue emptyStr(UniValue::VSTR);
+		arrayCreateParams.push_back(emptyStr);
 		request1.params = arrayCreateParams;	
 		resCreate = assetallocationsend(request1);
 		const UniValue &resArray = resCreate.get_array();
