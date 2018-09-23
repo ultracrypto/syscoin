f7fdebc23806d8e2748a98ac66ca2ee114b6412e
sidhujag <sidhujag@gmail.com>
2018-09-22 18:16:15 -0700

    update build script

--- contrib/macdeploy/custom_dsstore.py
+++ contrib/macdeploy/custom_dsstore.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2013-2016 The Bitcoin Core developers
-# Copyright (c) 2014-2018 The Syscoin Core developers
+# Copyright (c) 2013-2017 The Syscoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 import biplist
@@ -54,7 +53,7 @@ ds['.']['icvp'] = icvp
 ds['.']['vSrn'] = ('long', 1)
 
 ds['Applications']['Iloc'] = (370, 156)
-ds['Bitcoin-Qt.app']['Iloc'] = (128, 156)
+ds['Syscoin-Qt.app']['Iloc'] = (128, 156)
 
 ds.flush()
 ds.close()
