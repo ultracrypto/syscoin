b8a495628463878c48c1b3a3ef5400d6dbf10d50
sidhujag <sidhujag@gmail.com>
2018-09-23 20:49:02 -0700

    namespace

--- src/addrman.cpp
+++ src/addrman.cpp
@@ -9,7 +9,7 @@
 #include "hash.h"
 #include "serialize.h"
 #include "streams.h"
-
+using namespace std;
 int CAddrInfo::GetTriedBucket(const uint256& nKey) const
 {
     uint64_t hash1 = (CHashWriter(SER_GETHASH, 0) << nKey << GetKey()).GetHash().GetCheapHash();
