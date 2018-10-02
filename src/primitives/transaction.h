89cfbc7789f7bf222bf754798e3c376185d7d4bf
sidhujag <sidhujag@hotmail.com>
2018-10-02 10:07:19 -0700

    compile

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -11,7 +11,7 @@
 #include "script/script.h"
 #include "serialize.h"
 #include "uint256.h"
-#include "../alias.h"
+#include "services/alias.h"
 /** An outpoint - a combination of a transaction hash and an index n into its vout */
 class COutPoint
 {
