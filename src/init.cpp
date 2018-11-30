5dd50a9e871141947f12217c692a8ba75e3bd739
sidhujag <sidhujag@hotmail.com>
2018-11-30 15:54:47 -0800

    compile

--- src/init.cpp
+++ src/init.cpp
@@ -858,7 +858,7 @@ namespace { // Variables internal to initialization process only
 int nMaxConnections;
 int nUserMaxConnections;
 int nFD;
-ServiceFlags nLocalServices = ServiceFlags(NODE_NETWORK | NODE_NETWORK_LIMITED);
+ServiceFlags nLocalServices = ServiceFlags(NODE_NETWORK | NODE_NETWORK_LIMITED | NODE_WITNESS);
 
 } // namespace
 
@@ -1635,15 +1635,6 @@ bool AppInitMain()
             PruneAndFlush();
         }
     }
-
-	if (chainparams.GetConsensus().vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout != 0) {
-		// Only advertise witness capabilities if they have a reasonable start time.
-		// This allows us to have the code merged without a defined softfork, by setting its
-		// end time to 0.
-		// Note that setting NODE_WITNESS is never required: the only downside from not
-		// doing so is that after activation, no upgraded nodes will fetch from you.
-		nLocalServices = ServiceFlags(nLocalServices | NODE_WITNESS);
-	}
     // ********************************************************* Step 11: import blocks
 
     if (!CheckDiskSpace() && !CheckDiskSpace(0, true))
